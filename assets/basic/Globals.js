7df266100bae4fe2126e6785826e2afc324850e6
Mac <570768995@qq.com>
2018-04-28 17:59:07 +0800

    matchVS init

--- assets/basic/Globals.js
+++ assets/basic/Globals.js
@@ -1,3 +1,3 @@
 window.Game = {
-
+    GameManager: null
 }
--- assets/basic/gameManager.js
+++ assets/basic/gameManager.js
@@ -2,43 +2,9 @@ cc.Class({
     extends: cc.Component,
 
     onLoad() {
+        Game.GameManager = this;
         clientEvent.init();
         dataFunc.loadConfigs();
         uiFunc.openUI("uiMaskLayout");
-
-
-    },
-
-    networkExample: function() {
-        nano.init({
-            host: "127.0.0.1",
-            port: 3250,
-            path: '/nano',
-            reconnect: true
-        }, function() {
-            console.log("connect success by chenhao");
-            nano.request("room.join", {}, function(data) {
-                console.log(data);
-
-                nano.on('onMessage', onMessage);
-                nano.on("onMembers", onMembers);
-                nano.on("onNewUser", onNewUser);
-            });
-        });
-
-        function onMembers(data) {
-            console.log("onMembers");
-            console.log(data);
-        };
-
-        function onMessage(data) {
-            console.log("onMessage");
-            console.log(data);
-        };
-
-        function onNewUser(data) {
-            console.log(("On New User"));
-            console.log(data);
-        };
     }
 });
