3564a428de62a560d2631de34e621c85ea53e36f
Mac <570768995@qq.com>
2018-05-25 10:28:08 +0800

    fix

--- assets/basic/Glb.js
+++ assets/basic/Glb.js
@@ -1,23 +1,30 @@
 var obj = {
     RANDOM_MATCH: 1,  // 随机匹配
     PROPERTY_MATCH: 2,  // 属性匹配
-    MAX_PLAYER_COUNT: 3,
+    MAX_PLAYER_COUNT: 2,
     PLAYER_COUNTS: [1, 2, 3],
+    COOPERATION: 1,
+    COMPETITION: 2,
     GAME_START_EVENT: "gameStart",
-    NEW_START_EVENT: "newStar",
-    PLAYER_MOVE_EVENT: "playerMove",
-    GAIN_SCORE_EVENT: "gainScore",
-    PLAYER_POSITION_EVENT: "playerPosition",
+    GAME_TIME: "gameTime",
+    GAME_OVER_EVENT: "gameOver",
 
     channel: 'MatchVS',
     platform: 'alpha',
-    gameId: 200757,
-    gameVersion: 1,
-    appKey: '6783e7d174ef41b98a91957c561cf305',
-    secret: 'da47754579fa47e4affab5785451622c',
 
-    matchType: 1,
+    gameId: 201331,
+    gameVersion: 1,
+    appKey: '17ffc6d5f1e14a04b99c4bf17addc411',
+    secret: '4cc0d042cd5547e98860728bb3207650',
+
+    // gameId: 200757,
+    // gameVersion: 1,
+    // appKey: '6783e7d174ef41b98a91957c561cf305',
+    // secret: 'da47754579fa47e4affab5785451622c',
+
+
     gameType: 1,
+    matchType: 1,
     tagsInfo: { "title": "A" },
     userInfo: null,
     playerUserIds: [],
@@ -26,9 +33,9 @@ var obj = {
     events: {},
 
     syncFrame: false,
-    FRAME_RATE: 5,
+    FRAME_RATE: 20,
     roomId: 0,
     playertime: 180,
-    isGameOver: false,
+    isGameOver: false
 };
 module.exports = obj;
